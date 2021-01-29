75bba4ad88dab7eb69204f2d46b1eb7fb49edbb7
Tony Mogoa <mogoa.tonny@gmail.com>
2021-01-29 13:09:18 +0300

    Minor code refactoring

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-require_once("./model/Page.php");
+require_once("./model/Page.class.php");
 
 $page = new Page("Login");
 
 $page->set_header(require_once("./views/nav.php"));
-$page->set_body(require_once("./views/password_reset.php"));
+$page->set_body(require_once("./views/home.php"));
 $page->render();
 ?>
