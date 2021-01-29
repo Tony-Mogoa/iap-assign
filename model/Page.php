0d3a2405a106dffb9a7344e58beecb0d0cce91b4
Tony Mogoa <mogoa.tonny@gmail.com>
2021-01-29 11:42:48 +0300

    Directories refactored

---
+++ model/Page.php
@@ -0,0 +1,10 @@
+<?php
+class Page{
+    private $title;
+    private $style;
+    private $js;
+    private $header;
+    private $body;
+    private $footer;
+}
+?>
---
+++ views/page.php
@@ -0,0 +1,22 @@
+<?php
+return "<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset='UTF-8' />
+    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+    <link rel='preconnect' href='https://fonts.gstatic.com' />
+    <link
+      href='https://fonts.googleapis.com/css2?family=PT+Sans&display=swap'
+      rel='stylesheet'
+    />
+    <title>$this->title</title>
+    $this->style 
+    $this->js
+  </head>
+  <body>
+    $this->header 
+    $this->body 
+    $this->footer
+  </body>
+</html>
+"; ?>
